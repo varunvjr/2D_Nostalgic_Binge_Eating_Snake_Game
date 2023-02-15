231f169e04c90cbc1c2655859a0f0c3a3e0a9416
varunvjr <varun.vjr@gmail.com>
2023-02-14 21:50:13 -0800

    Snake Created

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
 import tkinter as tk
-
+import pygame
 # 2d Snake Game Board Dimensions
 
 WIDTH = 1000
 HEIGHT = 700
-
-
+BODY_SIZE = 200
+SPACE_SIZE = 20
 BACKGROUND = "#5af5aa"
+SNAKE = "#000000"
+class Snake:
+        def __init__(self):
+            pygame.init()
+         
+            self.body_size = BODY_SIZE
+            self.coordinates = []
+            self.squares = []
+            for i in range(0, BODY_SIZE):
+                self.coordinates.append([20, 20])
+            for x, y in self.coordinates:
+                square = canvas.create_rectangle(
+                    x, y, x + SPACE_SIZE, y + SPACE_SIZE,
+                        fill=SNAKE, tag="snake")
+                self.squares.append(square)
+
+def SnakeDisplay(snake):
+        x, y = snake.coordinates[100]
+        snake.coordinates.insert(0, (x, y))
+        square = canvas.create_rectangle(
+            x, y, x + SPACE_SIZE,
+                    y + SPACE_SIZE, fill=SNAKE)
+        snake.squares.insert(0, square)
+         
 
 # Create a new window
 window = tk.Tk()
@@ -22,7 +46,8 @@ canvas.pack()
 label = tk.Label(window, text="Binge Snake!")
 label.pack()
 
-
+snake = Snake()
+SnakeDisplay(snake)
 # Run the window
 window.update()
 window.mainloop()
